3816c413147b5b8f5bb1ea98d5a29becefe98fa2
nicolas-f <github@nettrader.fr>
2016-11-30 11:24:55 +0100

    Add breakpad code

--- wxminimal/minimal.cpp
+++ wxminimal/minimal.cpp
@@ -18,6 +18,7 @@
 
 // For compilers that support precompilation, includes "wx/wx.h".
 #include "wx/wxprec.h"
+#include <wx/dir.h>
 
 #ifdef __BORLANDC__
     #pragma hdrstop
@@ -38,6 +39,14 @@
 #ifndef wxHAS_IMAGES_IN_RESOURCES
     #include "../sample.xpm"
 #endif
+
+#ifdef _WIN32
+	#include "client/windows/crash_generation/client_info.h"
+	#include "client/windows/crash_generation/crash_generation_server.h"
+	#include "client/windows/handler/exception_handler.h"
+	#include "client/windows/common/ipc_protocol.h"
+	#include "client/windows/tests/crash_generation_app/abstract_class.h"
+#endif // _WIN32
 
 // ----------------------------------------------------------------------------
 // private classes
@@ -62,6 +71,8 @@ class MyFrame : public wxFrame
 public:
     // ctor(s)
     MyFrame(const wxString& title);
+
+	wxTextCtrl *MainEditBox;
 
     // event handlers (these functions should _not_ be virtual)
     void OnQuit(wxCommandEvent& event);
@@ -118,6 +129,25 @@ wxIMPLEMENT_APP(MyApp);
 // 'Main program' equivalent: the program execution "starts" here
 bool MyApp::OnInit()
 {
+	using namespace google_breakpad;
+
+	CustomClientInfo custom_info = { kCustomInfoEntries, kCustomInfoCount };
+
+    CrashServerStart();
+	// This is needed for CRT to not show dialog for invalid param
+	// failures and instead let the code handle it.
+	#ifdef _WIN32
+		_CrtSetReportMode(_CRT_ASSERT, 0);
+	#endif
+	handler = new ExceptionHandler(L"C:\\dumps\\",
+		NULL,
+		google_breakpad::ShowDumpResults,
+		NULL,
+		ExceptionHandler::HANDLER_ALL,
+		MiniDumpNormal,
+		kPipeName,
+		&custom_info);
+
     // call the base class initialization method, currently it only parses a
     // few common command-line options but it could be do more in the future
     if ( !wxApp::OnInit() )
@@ -147,36 +177,26 @@ MyFrame::MyFrame(const wxString& title)
     // set the frame icon
     SetIcon(wxICON(sample));
 
-#if wxUSE_MENUS
+	// Initialize our text box with an id of TEXT_Main, and the label "hi"
+	MainEditBox = new wxTextCtrl(this, wxID_ANY, "", wxDefaultPosition, wxDefaultSize,
+		wxTE_MULTILINE | wxTE_RICH, wxDefaultValidator, wxTextCtrlNameStr);
+
     // create a menu bar
     wxMenu *fileMenu = new wxMenu;
 
     // the "About" item should be in the help menu
     wxMenu *helpMenu = new wxMenu;
-    helpMenu->Append(Minimal_About, "&About\tF1", "Show about dialog");
+    helpMenu->Append(Minimal_About, wxT("&About\tF1"), wxT("Show about dialog"));
 
-    fileMenu->Append(Minimal_Quit, "E&xit\tAlt-X", "Quit this program");
+    fileMenu->Append(Minimal_Quit, wxT("E&xit\tAlt-X"), wxT("Quit this program"));
 
     // now append the freshly created menu to the menu bar...
     wxMenuBar *menuBar = new wxMenuBar();
-    menuBar->Append(fileMenu, "&File");
-    menuBar->Append(helpMenu, "&Help");
+    menuBar->Append(fileMenu, wxT("&File"));
+    menuBar->Append(helpMenu, wxT("&Help"));
 
     // ... and attach this menu bar to the frame
     SetMenuBar(menuBar);
-#else // !wxUSE_MENUS
-    // If menus are not available add a button to access the about box
-    wxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
-    wxButton* aboutBtn = new wxButton(this, wxID_ANY, "About...");
-    aboutBtn->Bind(wxEVT_BUTTON, &MyFrame::OnAbout, this);
-    sizer->Add(aboutBtn, wxSizerFlags().Center());
-#endif // wxUSE_MENUS/!wxUSE_MENUS
-
-#if wxUSE_STATUSBAR
-    // create a status bar just for fun (by default with 1 pane only)
-    CreateStatusBar(2);
-    SetStatusText("Welcome to wxWidgets!");
-#endif // wxUSE_STATUSBAR
 }
 
 
@@ -203,3 +223,237 @@ void MyFrame::OnAbout(wxCommandEvent& WXUNUSED(event))
                  wxOK | wxICON_INFORMATION,
                  this);
 }
+
+
+namespace google_breakpad {
+
+	const int kMaxLoadString = 100;
+	const wchar_t kPipeName[] = L"\\\\.\\pipe\\BreakpadCrashServices\\TestServer";
+	
+	// Maximum length of a line in the edit box.
+	const size_t kMaximumLineLength = 256;
+	
+	static size_t kCustomInfoCount = 2;
+	static CustomInfoEntry kCustomInfoEntries[] = {
+		CustomInfoEntry(L"prod", L"CrashTestApp"),
+		CustomInfoEntry(L"ver", L"1.0"),
+	};
+
+	static ExceptionHandler* handler = NULL;
+	static CrashGenerationServer* crash_server = NULL;
+
+
+	static void AppendTextToEditBox(TCHAR* text) {
+		SYSTEMTIME current_time;
+		GetLocalTime(&current_time);
+		TCHAR line[kMaximumLineLength];
+		int result = swprintf_s(line,
+			kMaximumLineLength,
+			L"[%.2d-%.2d-%.4d %.2d:%.2d:%.2d] %s",
+			current_time.wMonth,
+			current_time.wDay,
+			current_time.wYear,
+			current_time.wHour,
+			current_time.wMinute,
+			current_time.wSecond,
+			text);
+
+		if (result == -1) {
+			return;
+		}
+
+		// TODO set text line in window
+	}
+
+	bool ShowDumpResults(const wchar_t* dump_path,
+		const wchar_t* minidump_id,
+		void* context,
+		EXCEPTION_POINTERS* exinfo,
+		MDRawAssertionInfo* assertion,
+		bool succeeded) {
+		TCHAR* text = new TCHAR[kMaximumLineLength];
+		text[0] = wxT('\0');
+		int result = swprintf(text,
+			kMaximumLineLength,
+			TEXT("Dump generation request %s\r\n"),
+			succeeded ? wxT("succeeded") : wxT("failed"));
+		if (result == -1) {
+			delete[] text;
+		}
+
+		//TODO call AppendTextToEditBox
+		return succeeded;
+	}
+
+	static void ShowClientConnected(void* context,
+		const ClientInfo* client_info) {
+		TCHAR* line = new TCHAR[kMaximumLineLength];
+		line[0] = wxT('\0');
+		int result = swprintf(line,
+			kMaximumLineLength,
+			L"Client connected:\t\t%d\r\n",
+			client_info->pid());
+
+		if (result == -1) {
+			delete[] line;
+			return;
+		}
+
+		//TODO call AppendTextToEditBox
+	}
+
+	static void ShowClientCrashed(void* context,
+		const ClientInfo* client_info,
+		const wstring* dump_path) {
+		TCHAR* line = new TCHAR[kMaximumLineLength];
+		line[0] = wxT('\0');
+		int result = swprintf_s(line,
+			kMaximumLineLength,
+			wxT("Client requested dump:\t%d\r\n"),
+			client_info->pid());
+
+		if (result == -1) {
+			delete[] line;
+			return;
+		}
+
+		//TODO AppendTextToEditBox line
+
+		CustomClientInfo custom_info = client_info->GetCustomInfo();
+		if (custom_info.count <= 0) {
+			return;
+		}
+
+		wstring str_line;
+		for (size_t i = 0; i < custom_info.count; ++i) {
+			if (i > 0) {
+				str_line += L", ";
+			}
+			str_line += custom_info.entries[i].name;
+			str_line += L": ";
+			str_line += custom_info.entries[i].value;
+		}
+
+		line = new TCHAR[kMaximumLineLength];
+		line[0] = wxT('\0');
+		result = swprintf_s(line,
+			kMaximumLineLength,
+			L"%s\n",
+			str_line.c_str());
+		if (result == -1) {
+			delete[] line;
+			return;
+		}
+
+		//TODO AppendTextToEditBox line
+	}
+
+	static void ShowClientExited(void* context,
+		const ClientInfo* client_info) {
+		TCHAR* line = new TCHAR[kMaximumLineLength];
+		line[0] = wxT('\0');
+		int result = swprintf(line,
+			kMaximumLineLength,
+			wxT("Client exited:\t\t%d\r\n"),
+			client_info->pid());
+
+		if (result == -1) {
+			delete[] line;
+			return;
+		}
+
+		//TODO AppendTextToEditBox line
+	}
+
+	void CrashServerStart() {
+		// Do not create another instance of the server.
+		if (crash_server) {
+			return;
+		}
+
+		std::wstring dump_path = L"Dumps\\";
+
+		if (!wxDir::Make(dump_path)) {
+			wxMessageBox(wxT("Unable to create dump directory"), wxT("Dumper"), wxICON_ERROR);
+			return;
+		}
+
+		crash_server = new CrashGenerationServer(kPipeName,
+			NULL,
+			ShowClientConnected,
+			NULL,
+			ShowClientCrashed,
+			NULL,
+			ShowClientExited,
+			NULL,
+			NULL,
+			NULL,
+			true,
+			&dump_path);
+
+		if (!crash_server->Start()) {
+			wxMessageBox(wxT("Unable to start server"), wxT("Dumper"), wxICON_ERROR);
+			delete crash_server;
+			crash_server = NULL;
+		}
+	}
+
+	void CrashServerStop() {
+		delete crash_server;
+		crash_server = NULL;
+	}
+
+	void DerefZeroCrash() {
+		int* x = 0;
+		*x = 1;
+	}
+
+	void InvalidParamCrash() {
+		printf(NULL);
+	}
+
+	void PureCallCrash() {
+		Derived derived;
+	}
+
+	void RequestDump() {
+		if (!handler->WriteMinidump()) {
+			wxMessageBox(wxT("Dump request failed"), wxT("Dumper"), wxICON_ERROR);
+		}
+		kCustomInfoEntries[1].set_value(L"1.1");
+	}
+
+	void CleanUp() {
+
+		if (handler) {
+			delete handler;
+		}
+
+		if (crash_server) {
+			delete crash_server;
+		}
+	}
+	/*
+	
+			case ID_SERVER_START:
+				CrashServerStart();
+				break;
+			case ID_SERVER_STOP:
+				CrashServerStop();
+				break;
+			case ID_CLIENT_DEREFZERO:
+				DerefZeroCrash();
+				break;
+			case ID_CLIENT_INVALIDPARAM:
+				InvalidParamCrash();
+				break;
+			case ID_CLIENT_PURECALL:
+				PureCallCrash();
+				break;
+			case ID_CLIENT_REQUESTEXPLICITDUMP:
+				RequestDump();
+				break;
+	
+	*/
+
+}  // namespace google_breakpad
